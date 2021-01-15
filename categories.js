b88e7c5d9391f472c1a485843a12a3bf47baa56d
Thiago Brasil <thiagosbrasil@ua.pt>
2021-01-15 20:49:00 +0000

    categories 1.0

---
+++ categories.js
@@ -0,0 +1,9 @@
+function toggleBorder(object) {
+  object.classList.toggle("border");
+  object.classList.toggle("border-light");
+}
+
+function untoggleBorder() {
+  borda.classList.toggle("border");
+  borda.classList.toggle("border-light");
+}
