b962d539bb917934133a3a23f3bebede96902db3
juanlessa <juanvlessa@ua.pt>
2021-01-19 10:27:17 +0000

    actor.js changes

--- actors.js
+++ actors.js
@@ -1,6 +1,6 @@
-async function getActors() {
+async function getActors(current_page, current_pagesize) {
     const result = await $.ajax({
-        url: "http://192.168.160.58/netflix/api/Actors?page=1&pagesize=27000",
+        url: "http://192.168.160.58/netflix/api/Actors?page="+ current_page +"&pagesize="+ current_pagesize,
         type: "GET",
         success: function (data) {
             //wacky nested anonymous callbacks go here
@@ -9,39 +9,42 @@ async function getActors() {
             // Empty most of the time...
         },
     });
+    number_of_pages = result.TotalPages
     return result.Actors;
 }
 
-function changePage() {
+async function changePage() {
     //hide loader
     $("#conteudo").toggleClass("hide");
     $("#loader").toggleClass("hide-loader");
     
     pageToJump = event.target.text;
 
-    $("#page" + pageToJump).toggleClass("active");
-    $("#page" + current_page).toggleClass("active");
-
     var len = actors.list().length;
     for (let i = 0; i < len; i++) {
         actors.list.pop();
     }
+    $("#page" + pageToJump).toggleClass("active");
+    $("#page" + current_page).toggleClass("active");
 
-    
     current_page = pageToJump;
-
-    for (let i = 0; i < allactors.length; i++) {
-        var firstL = String(allactors[i].Name[0]);
-        if (pageToJump == firstL) {
-            actors.list.push(allactors[i]);
-        }
-    }
+    
+    actors_list = await getActors(current_page, current_pagesize);
+    actors_list.map((actor) => 
+        actors.list.push(actor)
+    );
 
     actors.list.shift();
+
     //hide loader
     $("#conteudo").toggleClass("hide");
     $("#loader").toggleClass("hide-loader");
 }
+//redirect
+function redirectPage(elem) {
+    var id = elem.id;
+    window.location.replace("actorTitles.html?id=" + id);
+  }
 
 //lista de categorias
 function ActorsViewModel() {
@@ -55,17 +58,22 @@ ko.applyBindings(actors);
 var allactors = [];
 
 //paginação
-var current_page = "A";
+var current_page = 1;
+var current_pagesize = 200;
+var number_of_pages = 1;
+
 $(document).ready(async function () {
     console.log("comecou");
-    allactors = await getActors();
-    for (let i = 0; i < allactors.length; i++) {
-        var firstL = String(allactors[i].Name[0]);
-        if ("A" == firstL) {
-            actors.list.push(allactors[i]);
-        }
-    }
+    actors_list = await getActors(current_page, current_pagesize);
+    actors_list.map((actor) => 
+        actors.list.push(actor)
+    );
+   
     console.log(actors.list());
+    
+    for (let i = 2; i <= 20; i++) {
+        $("#pageList").append( "<li id='page"+ i +"' class='page-item'><a class='page-link' href='#' onclick='changePage()'>"+i+"</a></li>" );
+    }
 
     //hide loader
     $("#conteudo").toggleClass("hide");
